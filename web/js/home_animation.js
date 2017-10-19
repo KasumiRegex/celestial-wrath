585b10a90ee180216da0043c60d80bc40ea0b2d4
werdelar <werdelar@students.zhaw.ch>
2017-10-19 10:52:55 +0200

    right version xD

---
+++ web/js/home_animation.js
@@ -0,0 +1,14 @@
+
+  $(function(){
+	  $("#home_type_effect").typed({
+		strings: ["we are more than just gamers"], 
+		contentType: 'html',
+		showCursor: false,
+		// time before typing starts
+		startDelay: 1200,
+		// backspacing speed
+		backSpeed: 30,
+		cursorChar: "_",
+		typeSpeed: 40
+	  });
+  });
